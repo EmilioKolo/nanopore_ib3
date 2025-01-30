63c4d20b3c2f891feee49d2a99be711ae2a0ca0b
EmilioKolo <emilio_193@hotmail.com>
2025-01-30 11:28:23 -0300

    Update variant_filtering.py
    Add process_info and process_sample to open_vcf function

--- variant_filtering.py
+++ variant_filtering.py
@@ -56,10 +56,64 @@ def open_vcf(vcf_name, vcf_path='.', ext='.vcf', sep='\t'):
                 dict_line = {}
                 # Go through values in cl
                 for i in range(len(cl)):
-                    # Load each value to dict_line
-                    dict_line[coltitles[i]] = cl[i]
+                    if coltitles[i]=='INFO':
+                        dict_line['INFO'] = process_info(cl[i])
+                    elif coltitles[i]=='FORMAT':
+                        pass
+                    elif coltitles[i]=='SAMPLE':
+                        cl_samp = cl[coltitles.index('SAMPLE')]
+                        cl_format = cl[coltitles.index('FORMAT')]
+                        dict_line['SAMPLE'] = process_sample(
+                            cl_samp,
+                            cl_format
+                            )
+                    else:
+                        # Load most values directly to dict_line
+                        dict_line[coltitles[i]] = cl[i]
                 # Load dict_line to m_out
                 m_out.append(copy.deepcopy(dict_line))
+    return m_out
+
+
+def process_info(info_str):
+    """
+    Processes the INFO column of a VCF file.
+    """
+    # Initialize output variable
+    m_out = {}
+    # Split info_str by semicolon
+    l_info = info_str.split(';')
+    # Go through l_info
+    for i in l_info:
+        # Split by equal sign
+        l_i = i.split('=')
+        # Check if l_i has two values
+        if len(l_i)==2:
+            # Load normally to m_out
+            m_out[l_i[0]] = l_i[1]
+        elif len(l_i)<2:
+            # Load None to m_out
+            m_out[l_i[0]] = None
+        else:
+            # Load to m_out but print warning
+            m_out[l_i[0]] = l_i[1]
+            print(f'WARNING: {l_i} has more than two values.')
+    return m_out
+
+def process_sample(sample_str, format_str):
+    """
+    Processes the SAMPLE column of a VCF file.
+    """
+    # Initialize output variable
+    m_out = {}
+    # Split sample_str by colon
+    l_sample = sample_str.split(':')
+    # Split format_str by colon
+    l_format = format_str.split(':')
+    # Go through l_sample
+    for i in range(len(l_sample)):
+        # Load to m_out
+        m_out[l_format[i]] = l_sample[i]
     return m_out
 
 
