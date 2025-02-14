02845f16649783ada5877df1d150d2a4cc5e3a22
EmilioKolo <emilio_193@hotmail.com>
2025-02-14 11:46:34 -0300

    Add adapter info to basecall and barcode

--- scripts/basecall_barcode.py
+++ scripts/basecall_barcode.py
@@ -5,7 +5,13 @@ from scripts.file_manager import mkdir_p, get_value
 # Important directories
 INPUT_DIR = get_value('input_dir')
 OUTPUT_DIR = get_value('output_dir')
+ADAP_DIR = get_value('adap_dir')
 THREADS = get_value('thread_n')
+ADAPTERS = [ # Default picks first
+    'TruSeq3-PE-3.fa',
+    'TruSeq3-PE-2.fa',
+    'TruSeq3-PE.fa'
+    ]
 
 
 def _main():
