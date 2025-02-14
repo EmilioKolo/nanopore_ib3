510dbccede505db9f3b7d2887b86875b6b36a2e7
EmilioKolo <emilio_193@hotmail.com>
2025-02-14 11:33:37 -0300

    Create basecall_barcode.py

---
+++ scripts/basecall_barcode.py
@@ -0,0 +1,16 @@
+
+import os
+from scripts.file_manager import mkdir_p, get_value
+
+# Important directories
+INPUT_DIR = get_value('input_dir')
+OUTPUT_DIR = get_value('output_dir')
+THREADS = get_value('thread_n')
+
+
+def _main():
+    return 0
+
+
+if __name__=='__main__':
+    _main()
