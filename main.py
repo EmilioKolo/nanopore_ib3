930ba345c28196c4f3ef4f96f2904bc7bc60d8b2
EmilioKolo <emilio_193@hotmail.com>
2025-02-16 20:06:01 -0300

    Create main.py
    Create script file to run the entire pipeline

---
+++ main.py
@@ -0,0 +1,11 @@
+
+from scripts.basecall_barcode import basecall_barcode_one
+from scripts.variant_calling import pipeline_variant_call
+
+
+def _main():
+    return 0
+
+
+if __name__=='__main__':
+    _main()
