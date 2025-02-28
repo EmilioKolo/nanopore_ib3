9a983197c4425c5348b7dd2cb74250769b6e22a4
EmilioKolo <emilio_193@hotmail.com>
2025-02-28 14:24:04 -0300

    Update main.py
    Add pipeline basecall_barcode to _main

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 
-from scripts.basecall_barcode import basecall_barcode_one
+from scripts.basecall_barcode import pipeline_basecall_barcode
+from scripts.file_manager import open_txt, get_value
 from scripts.variant_calling import pipeline_variant_call
 
 
+INPUT_DIR = get_value('input_dir')
+OUTPUT_DIR = get_value('output_dir')
+INPUT_FILE = get_value('input_file')
+
+
 def _main():
+    # Open the file with open_txt
+    l_files = open_txt(INPUT_FILE, INPUT_DIR, header=False, skip='#')
+    # Run the pipeline
+    pipeline_basecall_barcode(l_files, INPUT_DIR, out_path=OUTPUT_DIR)
     return 0
 
 
